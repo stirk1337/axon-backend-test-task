f15abccfbf5fbdc8bc9c36c7ee61d9056027d2f1
stirk <stirk-delovoy@mail.ru>
2024-03-07 14:27:55 +0500

    feat: Add pgadmin into docker compose

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     """ Class with settings and configs for the project """
-    model_config = SettingsConfigDict(env_file='.env')
+    model_config = SettingsConfigDict(env_file='.env', extra='ignore')
 
     postgres: PostgresDsn
     postgres_test: PostgresDsn
